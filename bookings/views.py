8f184b722fb0c03d6d8259a7f84c7b844d2757b9
deepakdev <deepakp.tvla@gmail.com>
2024-09-07 21:03:21 +0000

    Implemented user authentication

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Booking
 
-# Create your views here.
+def booking_list(request):
+    bookings = Booking.objects.all().order_by('date', 'time')
+    return render(request, 'bookings/booking_list.html', {'bookings': bookings})
+
+def home(request):
+    return render(request, 'bookings/home.html')
